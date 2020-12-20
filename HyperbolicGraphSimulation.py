9fbd88ae77a4dc8ebbbcdb407e0f66cf4f35f1ea
LourensT <touwenlourens@gmail.com>
2020-12-20 14:48:30 +0100

    include setFolder method

--- HyperbolicGraphSimulation.py
+++ HyperbolicGraphSimulation.py
@@ -6,17 +6,24 @@ import networkx as nx
 import network2tikz
 import matplotlib.pyplot as plt
 import datetime
+import os
 
 class HyperBolicGraphSimulation:
 
-    def __init__(self, average_degrees, negative_curvature):
-        self.v = average_degrees
-        self.a = negative_curvature
+    def __init__(self):
         #bulkwise sampler is quicker then sampling one by one
         self.uniformSampler = Distribution(stats.uniform()) 
+        self.dir = ""
 
-    def generateGraph(self, n):
+    def setOutputFolder(self, dir):
+        assert os.path.isdir(dir), "checking if valid folder"
+        self.dir = dir 
+
+    def generateGraph(self, n, average_degrees, negative_curvature):
+        self.v = average_degrees
+        self.a = negative_curvature
         self.R = 2*np.log(n/self.v)
+
         self.samplePoints(n)
 
         self.G = nx.Graph()
@@ -75,21 +82,22 @@ class HyperBolicGraphSimulation:
     '''
     output to tex and display graph
     '''
-    def draw(self, fp="", style=""):
+    def draw(self, fp="", style="", show = False):
         #get default filepath
         if fp == "":
-            fp = datetime.datetime.now().strftime("%Hh%Mm%Ss %d-%m-%Y") +".tex"
+            fp = datetime.datetime.now().strftime("%Hh%Mm%Ss %d-%m-%Y") +".tikz"
         
         #get the positions of all nodes in euclidian coordinates
         layout = {k : self.convertPolarToEuclidian(self.pointsPositions[k]) for k in self.pointsPositions.keys()}
         #convert the network to tikz
-        network2tikz.plot(self.G, fp, layout=layout, canvas=(10,10))
+        network2tikz.plot(self.G, self.dir+fp.replace('.tikz', '.tex'), layout=layout, canvas=(10,10))
         self.addStyleString(fp, style)
 
         #display the matplotlib view of the graph
-        nx.draw(self.G, pos=layout)
-        plt.grid(True)
-        plt.show()
+        if show:
+            nx.draw(self.G, pos=layout)
+            plt.grid(True)
+            plt.show()
 
     '''
     add the given style options to the output.tex at appropriate place
@@ -98,20 +106,39 @@ class HyperBolicGraphSimulation:
 
         #read in the output file and interlace the styling options
         allLines = []
-        with open(fp, "r") as f:
+        shouldSave = False #filter to only save the tikz picture
+        with open(self.dir+fp.replace('.tikz', '.tex'), "r") as f:
             for line in f:
-                allLines.append(line)
-                if "begin{tikzpicture}" in line:
-                    for style_line in style:
-                        allLines.append(style_line)
+                if not shouldSave:
+                    if "begin{tikzpicture}" in line: #start of tikzpicutre
+                        for style_line in style:
+                            allLines.append(style_line)
+                        shouldSave = True
+                if shouldSave:
+                    allLines.append(line)
+                    if "end{tikzpicture}" in line: #end of tikzpicutre
+                        shouldSave = False
 
         #output the file again
-        with open(fp, "w") as f:
+        with open(self.dir+fp.replace('.tex', '.tikz'), "w") as f:
             for line in allLines:
-                f.write(line)   
+                f.write(line)
+
+        #remove original .tex file
+        os.remove(self.dir+fp.replace('.tikz', '.tex'))   
 
 if __name__ == "__main__":
-    style = ["\SetVertexStyle[MinSize=0.3\DefaultUnit, LineWidth=0pt, FillColor=vertexfill]\n", "\SetEdgeStyle[LineWidth=0.25pt]\n"]
-    simulator = HyperBolicGraphSimulation(1, 0.6)
-    simulator.generateGraph(250)
-    simulator.draw(style=style)
+    style = ["\SetVertexStyle[MinSize=0.3\DefaultUnit, LineWidth=0pt, FillColor=mycolor1]\n", "\SetEdgeStyle[LineWidth=0.25pt]\n"]
+    simulator = HyperBolicGraphSimulation()
+    simulator.setOutputFolder(os.getcwd()+ "//output_batch_1//" )
+
+    nrOfPoints = 250
+    avg_degrees = [0.5, 1, 1.5, 2, 2.5, 3, 3.5]
+    neg_curvature = [0.2, 0.6, 0.8, 1, 1.2, 1.4, 1.6]
+
+    for combination in itertools.product(avg_degrees, neg_curvature):
+        filepath = "n{}_v{}_a{}.tikz".format(nrOfPoints, str(combination[0]).replace(".", ","), str(combination[1]).replace(".", ","))
+        print(filepath)
+    
+        simulator.generateGraph(nrOfPoints, combination[0], combination[1])    
+        simulator.draw(style=style, fp=filepath)
